cde813ae00dcdd78f3663ac794102ec3eff48b84
Atefe Komeili <atefekomeili91299@gmail.com>
2025-03-10 14:13:46 +0100

    Add AI app to project configuration and URL routing
    - Registered AI app in Django project settings
    - Included AI app URLs in the main project URL configuration

--- memobox/urls.py
+++ memobox/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('leitner.urls')),
+    path('api/', include('ai.urls')),
 
 ]
